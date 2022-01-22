8eba68f951d4a50a0189474137ed756d1fba08ba
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-01-22 19:12:39 -0300

    Adicionado service de Pagamento e adicionado método no repository de Produto

--- src/main/java/com/joshaby/springboot2backend/repositories/ProdutoRepository.java
+++ src/main/java/com/joshaby/springboot2backend/repositories/ProdutoRepository.java
@@ -1,7 +1,22 @@
 package com.joshaby.springboot2backend.repositories;
 
+import com.joshaby.springboot2backend.entities.Categoria;
 import com.joshaby.springboot2backend.entities.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+    @Query("SELECT DISTINCT produto FROM Produto produto " +
+            "INNER JOIN produto.categorias categorias " +
+            "WHERE produto.nome LIKE %:nome% AND categorias in :categorias")
+    Page<Produto> findDistinctByNomeContainingAndCategoriasIN(
+            @Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageable);
 }
