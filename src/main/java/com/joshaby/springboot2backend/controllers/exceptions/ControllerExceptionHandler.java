621dc3f06962de3c57dda94db936c334dadb7217
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-02-16 21:37:42 -0300

    Adicionado manipuladores de exceções

---
+++ src/main/java/com/joshaby/springboot2backend/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.joshaby.springboot2backend.controllers.exceptions;
+
+import com.joshaby.springboot2backend.services.exceptions.DataIntegrityException;
+import com.joshaby.springboot2backend.services.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandartError> objectNotFound(ObjectNotFoundException exception) {
+
+        StandartError error = new StandartError(
+                HttpStatus.NOT_FOUND.value(), exception.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandartError> dataIntegrity(DataIntegrityException exception) {
+
+        StandartError error = new StandartError(
+                HttpStatus.BAD_REQUEST.value(), exception.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> dataValidation(MethodArgumentNotValidException exception) {
+
+        ValidationError error = new ValidationError(
+                HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+        for (FieldError fieldError : exception.getFieldErrors()) {
+            error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(error);
+    }
+}
