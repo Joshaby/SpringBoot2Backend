a425ffa8d9f46391fee132b691d7c3dde4c255b5
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-02-20 21:27:08 -0300

    Adicionado configurações básicas de segurança

---
+++ src/main/java/com/joshaby/springboot2backend/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.joshaby.springboot2backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] PUBLIC_MATCHES = {
+            "/h2-console/**"
+    };
+
+    private static final String[] PUBLIC_MATCHES_GET = {
+            "/produtos/**",
+            "/categorias/**"
+    };
+
+    private static final String[] PUBLIC_MATCHES_POST = {
+            "/clientes/**",
+            "/auth/forgot/**"
+    };
+
+    @Autowired
+    private Environment environment;
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            security.headers().frameOptions().disable();
+        }
+        security.cors().and().csrf().disable();
+        security.authorizeRequests()
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHES_POST).permitAll()
+                .antMatchers(HttpMethod.GET, PUBLIC_MATCHES_GET).permitAll()
+                .antMatchers(PUBLIC_MATCHES).permitAll()
+                .anyRequest().authenticated();
+        security.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "OPTIONS"));
+        final UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**", configuration);
+        return configurationSource;
+    }
+}
