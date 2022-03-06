dc0a2033c6d1240fb0cdfcf70956f3f0f913200a
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-03-06 19:28:18 -0300

    Adicionado JSON de resposta quando ocorre erro de autenticação

--- src/main/java/com/joshaby/springboot2backend/security/JWTAuthenticationFilter.java
+++ src/main/java/com/joshaby/springboot2backend/security/JWTAuthenticationFilter.java
@@ -3,10 +3,13 @@ package com.joshaby.springboot2backend.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.joshaby.springboot2backend.dto.CredenciasDTO;
 import com.joshaby.springboot2backend.security.util.JWTUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -14,7 +17,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -25,6 +30,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public JWTAuthenticationFilter(AuthenticationManager manager, JWTUtil jwtUtil) {
         this.manager = manager;
         this.jwtUtil = jwtUtil;
+        setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
     }
 
     @Override
@@ -49,5 +55,30 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         String username = ((UserDetailsImpl) authentication.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
         response.addHeader("Authorization", "Bearer " + token);
+    }
+
+    private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+        @Override
+        public void onAuthenticationFailure(
+                HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+                throws IOException, ServletException {
+
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+            response.getWriter().append(json());
+        }
+
+        private String json() {
+            long date = new Date().getTime();
+
+            return "{\"timestamp\": " + date + ", " +
+                    "\"status\": 401" + ", " +
+                    "\"error\": \"Não autorizado\"" + ", " +
+                    "\"message\": \"Email ou senha inválidos\"" + ", " +
+                    "\"path\": \"/login\"" +
+                    "}";
+        }
     }
 }
